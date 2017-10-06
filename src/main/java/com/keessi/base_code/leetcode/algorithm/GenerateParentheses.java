e4d44561a4a7bc416db94e4d9a4dece4ea654936
SPC1996 <1594475695@qq.com>
2017-10-06 20:27:24 +0800

    leetcode 22

---
+++ src/main/java/com/keessi/base_code/leetcode/algorithm/GenerateParentheses.java
@@ -0,0 +1,42 @@
+package com.keessi.base_code.leetcode.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * <p>
+ * For example, given n = 3, a solution set is:
+ * <p>
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+    private static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(res, "", 0, 0, n);
+        return res;
+    }
+
+    private static void backtrack(List<String> list, String str, int open, int close, int max) {
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+        if (open < max) {
+            backtrack(list, str + '(', open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(list, str + ')', open, close + 1, max);
+        }
+    }
+}
